8b389bc64d9227e7f94e341e61de4ab8f44fbbe5
מיקה רוטמן <MikaRotman@h-MacBook-Air-sl-rwmy.local>
2026-01-10 20:06:49 +0200

    Implement User and Network methods

--- Network.java
+++ Network.java
@@ -121,10 +121,10 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-       String ans = "";
+       String ans = "Network:";
         for (int i = 0; i<userCount; i++){
-            ans = ans + users[i].toString() + "\n" ;
+            ans += "\n" + users[i].toString();
         }
-        return   "Network:\n" + ans;
+        return ans;
     }
 }
